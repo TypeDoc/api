3b3b1c85a5cb8b93d6ecc5ae3e4cb2516b2e1a19
Sebastian Lenz <github@sebastian-lenz.de>
2015-03-08 11:21:55 +0100

    Switch to own grunt task

--- gruntfile.js
+++ gruntfile.js
@@ -1,9 +1,6 @@
 module.exports = function (grunt) {
     'use strict';
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-gh-pages');
-
     function getTypeDocPath() {
         var fs = require('fs');
         return fs.existsSync('./local.json') ? require('./local.json').typeDocPath : 'typedoc/src/';
@@ -17,6 +14,19 @@ module.exports = function (grunt) {
                 dest: 'dist/'
             }
         },
+        'typedoc': {
+            options: {
+                out: 'dist',
+                name: 'TypeDoc API Documentation',
+                hideGenerator: true,
+                readme: 'none',
+                gaID: 'UA-53674298-1',
+                gaSite: 'typedoc.io',
+                theme: 'theme',
+                mode: 'File'
+            },
+            src: getTypeDocPath()
+        },
         'gh-pages': {
             'dist': {
                 options: {
@@ -27,36 +37,11 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerTask('typedoc', function() {
-        var Path = require('path');
-        var TypeDoc = require('typedoc');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('typedoc');
 
-        var settings = new TypeDoc.Settings();
-        settings.inputFiles.push(getTypeDocPath());
-        settings.expandInputFiles();
-
-        settings.out           = Path.resolve('./dist/');
-        settings.name          = 'TypeDoc API Documentation';
-        settings.hideGenerator = true;
-        settings.readme        = 'none';
-        settings.gaID          = 'UA-53674298-1';
-        settings.gaSite        = 'typedoc.io';
-        settings.theme         = 'theme';
-        settings.mode          = TypeDoc.SourceFileMode.File;
-
-        var app = new TypeDoc.Application(settings);
-        app.generate(settings.inputFiles, settings.out);
-    });
-
-    grunt.registerTask('build', 'Create api documentation', [
-        'typedoc',
-        'copy'
-    ]);
-
-    grunt.registerTask('publish', 'Publish to typedoc.io/api', [
-        'build',
-        'gh-pages'
-    ]);
-
+    grunt.registerTask('build', 'Create api documentation', ['typedoc', 'copy']);
+    grunt.registerTask('publish', 'Publish to typedoc.io/api', ['build', 'gh-pages']);
     grunt.registerTask('default', ['build']);
 };
